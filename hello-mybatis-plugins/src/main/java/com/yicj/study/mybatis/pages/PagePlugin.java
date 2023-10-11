e3a861b335fbeca131f930ccf46fa5137c0077f6
易成杰 <626659321@qq.com>
2023-10-11 22:14:35 +0800

    test page plugins

--- hello-mybatis-plugins/src/main/java/com/yicj/study/mybatis/pages/PagePlugin.java
+++ hello-mybatis-plugins/src/main/java/com/yicj/study/mybatis/pages/PagePlugin.java
@@ -116,19 +116,14 @@ public class PagePlugin implements Interceptor {
         return ivk.proceed();
     }
 
-    private void setParameters(PreparedStatement ps,
-            MappedStatement mappedStatement, BoundSql boundSql,
-            Object parameterObject) throws SQLException {
-        ErrorContext.instance().activity("setting parameters")
-                .object(mappedStatement.getParameterMap().getId());
-        List<ParameterMapping> parameterMappings = boundSql
-                .getParameterMappings();
+    private void setParameters(
+            PreparedStatement ps, MappedStatement mappedStatement, BoundSql boundSql, Object parameterObject) throws SQLException {
+        ErrorContext.instance().activity("setting parameters").object(mappedStatement.getParameterMap().getId());
+        List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
         if (parameterMappings != null) {
             Configuration configuration = mappedStatement.getConfiguration();
-            TypeHandlerRegistry typeHandlerRegistry = configuration
-                    .getTypeHandlerRegistry();
-            MetaObject metaObject = parameterObject == null ? null
-                    : configuration.newMetaObject(parameterObject);
+            TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
+            MetaObject metaObject = parameterObject == null ? null : configuration.newMetaObject(parameterObject);
             for (int i = 0; i < parameterMappings.size(); i++) {
                 ParameterMapping parameterMapping = parameterMappings.get(i);
                 if (parameterMapping.getMode() != ParameterMode.OUT) {
@@ -137,34 +132,24 @@ public class PagePlugin implements Interceptor {
                     PropertyTokenizer prop = new PropertyTokenizer(propertyName);
                     if (parameterObject == null) {
                         value = null;
-                    } else if (typeHandlerRegistry
-                            .hasTypeHandler(parameterObject.getClass())) {
+                    } else if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())) {
                         value = parameterObject;
                     } else if (boundSql.hasAdditionalParameter(propertyName)) {
                         value = boundSql.getAdditionalParameter(propertyName);
-                    } else if (propertyName
-                            .startsWith(ForEachSqlNode.ITEM_PREFIX)
-                            && boundSql.hasAdditionalParameter(prop.getName())) {
+                    } else if (propertyName.startsWith(ForEachSqlNode.ITEM_PREFIX) && boundSql.hasAdditionalParameter(prop.getName())) {
                         value = boundSql.getAdditionalParameter(prop.getName());
                         if (value != null) {
-                            value = configuration.newMetaObject(value)
-                                    .getValue(
-                                            propertyName.substring(prop
-                                                    .getName().length()));
+                            value = configuration.newMetaObject(value).getValue(propertyName.substring(prop.getName().length()));
                         }
                     } else {
-                        value = metaObject == null ? null : metaObject
-                                .getValue(propertyName);
+                        value = metaObject == null ? null : metaObject.getValue(propertyName);
                     }
                     TypeHandler typeHandler = parameterMapping.getTypeHandler();
                     if (typeHandler == null) {
-                        throw new ExecutorException(
-                                "There was no TypeHandler found for parameter "
-                                        + propertyName + " of statement "
-                                        + mappedStatement.getId());
+                        throw new ExecutorException("There was no TypeHandler found for parameter "
+                                        + propertyName + " of statement " + mappedStatement.getId());
                     }
-                    typeHandler.setParameter(ps, i + 1, value,
-                            parameterMapping.getJdbcType());
+                    typeHandler.setParameter(ps, i + 1, value, parameterMapping.getJdbcType());
                 }
             }
         }
