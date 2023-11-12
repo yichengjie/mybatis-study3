afd4c9083754cd52bad9bb3d03f94fd4b18f0e9d
易成杰 <626659321@qq.com>
2023-11-12 20:15:50 +0800

    自动生request与response对象

---
+++ order-service/src/main/java/com/yicj/study/order/web/request/PageUserRequest.java
@@ -0,0 +1,17 @@
+package com.yicj.study.order.web.request;
+
+import com.yicj.study.common.model.request.PageRequest;
+import lombok.Data;
+
+/**
+* <p>
+*  分页查询Request
+* </p>
+*
+* @author yicj
+* @since 2023-11-12
+*/
+@Data
+public class PageUserRequest extends PageRequest {
+
+}
