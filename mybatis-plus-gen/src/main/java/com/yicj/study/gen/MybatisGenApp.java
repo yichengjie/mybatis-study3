fac0e0022180c038e701e599a826766e141483a3
易成杰 <626659321@qq.com>
2023-11-12 14:24:02 +0800

    补充数据源信息 + 单元测试

--- mybatis-plus-gen/src/main/java/com/yicj/study/gen/MybatisGenApp.java
+++ mybatis-plus-gen/src/main/java/com/yicj/study/gen/MybatisGenApp.java
@@ -49,7 +49,12 @@ public class MybatisGenApp {
                 })
                 .strategyConfig(builder -> {
                     builder.addInclude("t_user") // 设置需要生成的表名
-                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
+                            .addTablePrefix("t_", "c_")
+                            .entityBuilder().enableFileOverride()
+                            .mapperBuilder().enableFileOverride()
+                            .serviceBuilder().enableFileOverride()
+                            .controllerBuilder().enableFileOverride()
+                    ; // 设置过滤表前缀
 
                 })
                 .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
